d3dbf13d364e56e9123c651be0f33420a2f0bd08
aaronfeingold <ajfeingold88@gmail.com>
2020-10-15 18:51:33 -0500

    removed redunant things

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,23 +8,9 @@ import { IconContext } from 'react-icons/lib';
 function Navbar() {
 
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  }
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
 
   return(
     <>
