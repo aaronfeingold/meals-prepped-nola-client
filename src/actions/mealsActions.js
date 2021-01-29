94ad9d90e916874f596f94a1e0abe5e6f2b81248
aaronfeingold <ajfeingold88@gmail.com>
2021-01-29 00:59:40 -0600

    base url change

--- src/actions/mealsActions.js
+++ src/actions/mealsActions.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3001";
+// const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://meals-prepped-backend.herokuapp.com/";
 
 const LOADING = { type: "LOADING" };
 const SET_EDITING = { type: "SET_EDITING" };
